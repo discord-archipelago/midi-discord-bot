ed715d2d1980cbcb6013e467dd2083f61636c54d
HEEWON0010 <chw001025@gmail.com>
2026-09-02 20:19:05 +0900

    tweak: /똥짤 설명/안내 문구 수정

--- commands/dungjjal.js
+++ commands/dungjjal.js
@@ -2,12 +2,12 @@ import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
 import { getDungjjal } from "../utils/store.js";
 
 export default {
-  data: new SlashCommandBuilder().setName("똥짤").setDescription("등록된 똥짤 중 하나를 랜덤으로 보여줌!"),
+  data: new SlashCommandBuilder().setName("똥짤").setDescription("으 똥짤"),
 
   async execute(interaction) {
     const data = getDungjjal();
     if (data.images.length === 0) {
-      return interaction.reply("아직 등록된 똥짤이 없음! /등록 으로 먼저 등록해줘.");
+      return interaction.reply("아직 등록된 똥짤이 없음; /등록 으로 먼저 등록해줘.");
     }
 
     const pick = data.images[Math.floor(Math.random() * data.images.length)];
