3867522c3190073afc9956a34a0b0aa0a5f55158
HEEWON0010 <chw001025@gmail.com>
2026-09-02 20:19:11 +0900

    feat: /할거추천 목록 확장 및 등록된 항목 반영

--- commands/activity.js
+++ commands/activity.js
@@ -1,13 +1,15 @@
 import { SlashCommandBuilder } from "discord.js";
+import { getExtraActivities } from "../utils/store.js";
 
-// 할거 후보 목록 (직접 채워 넣기)
-const ACTIVITIES = ["산책하기", "낮잠 자기", "영화 보기", "게임하기", "청소하기"];
+// 할거 후보 목록 (직접 채워 넣기, /등록의 "할거 등록"으로 추가된 것도 합쳐짐)
+const ACTIVITIES = ["산책하기", "낮잠 자기", "영화 보기", "게임하기", "청소하기", "요리하기", "책 읽기", "음악 듣기", "운동하기", "그림 그리기", "사진 찍기", "글쓰기", "퍼즐 맞추기", "명상하기", "친구에게 연락하기", "새로운 취미 배우기", "노래방 가기", "공부하기", "디코 끄기"];
 
 export default {
-  data: new SlashCommandBuilder().setName("할거추천").setDescription("지금 뭐 할지 추천해줌!"),
+  data: new SlashCommandBuilder().setName("할거추천").setDescription("할게 없다고? 내가 추천해줄게!"),
 
   async execute(interaction) {
-    const activity = ACTIVITIES[Math.floor(Math.random() * ACTIVITIES.length)];
-    await interaction.reply(`지금은 **${activity}** 어때?`);
+    const pool = [...ACTIVITIES, ...getExtraActivities()];
+    const activity = pool[Math.floor(Math.random() * pool.length)];
+    await interaction.reply(`**${activity}**는(은) 어때?`);
   },
 };
