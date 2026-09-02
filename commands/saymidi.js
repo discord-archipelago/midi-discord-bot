b463081704bbc6b56a88709b114627c2f2404fbd
HEEWON0010 <chw001025@gmail.com>
2026-09-02 20:19:10 +0900

    tweak: /saymidi ephemeral 응답 제거

--- commands/saymidi.js
+++ commands/saymidi.js
@@ -3,14 +3,13 @@ import { SlashCommandBuilder } from "discord.js";
 export default {
   data: new SlashCommandBuilder()
     .setName("saymidi")
-    .setDescription("미디가 대신 말해줌 (명령어 사용 표시 없이)")
+    .setDescription("미디가 대신 말해줌")
     .addStringOption(opt =>
       opt.setName("메시지").setDescription("미디가 말할 문장").setRequired(true)
     ),
 
   async execute(interaction) {
     const message = interaction.options.getString("메시지");
-    await interaction.reply({ content: "말해줬어!", ephemeral: true });
     await interaction.channel.send(message);
   },
 };
