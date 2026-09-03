697d3780d9840c982523183895bf0da582679f6c
HEEWON0010 <chw001025@gmail.com>
2026-09-03 18:45:13 +0900

    feat: 욕설 목록 추가

--- utils/profanity.js
+++ utils/profanity.js
@@ -1,7 +1,7 @@
 import { getUsers, saveUsers, getUser } from "./store.js";
 
-// 감지할 욕설 목록 (직접 채워 넣기)
-const BANNED_WORDS = [];
+// 감지할 욕설 목록
+const BANNED_WORDS = ['시발', '씨발', '개새끼', '좆같네', '병신', '미친놈', '존나', '개같네', '좆', '개새끼야', '병신같은', '미친놈아', '존나게', '좆같이', '개같이', '씨발놈', '시발놈', '좆밥', '개새끼들', '병신새끼', '미친놈들', ];
 
 export async function handleProfanity(msg) {
   if (BANNED_WORDS.length === 0) return;
